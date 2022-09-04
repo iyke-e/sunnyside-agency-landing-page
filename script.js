65fc67b5d89f870cbdc9c337697d38d8d2b92099
IYKE <106726596+iyke-e@users.noreply.github.com>
2022-09-04 07:04:10 +0100

    complete project

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuBar = document.getElementById('menu');
+const navBar = document.getElementById('nav');
+const navList = document.querySelectorAll('#nav li')
+
+menu.addEventListener('click', () => { navBar.classList.toggle('active') });
